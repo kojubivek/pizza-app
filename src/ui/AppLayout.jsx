d237c9cc13c1de3464f7370e5deb4b92e0079dfe
kojubivek <kojubivek"gmail.com>
2023-12-13 14:29:05 +1100

    created app layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>content</h1>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
