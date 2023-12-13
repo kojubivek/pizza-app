eb34009946a3cc009b4981b8d4d3403413ebc8c7
kojubivek <kojubivek"gmail.com>
2023-12-13 16:08:09 +1100

    created react-router-dataloading

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loading from "./Loading";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loading />}
+
       <Header />
+      <SearchOrder />
+
       <main>
         <h1>content</h1>
         <Outlet />
